92e8468dbede88d37465e375766822d73afb91c1
MD. SHAHIDUL ISLAM <shahidul7889@gmail.com>
2018-05-29 21:20:01 +0600

    First Commit

---
+++ RenameLink.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WBrowser
+{
+    public partial class RenameLink : Form
+    {
+        String oldName;
+
+        public RenameLink(string oldName)
+        {
+            this.oldName = oldName;
+            InitializeComponent();
+        }
+
+        private void RenameLink_Load(object sender, EventArgs e)
+        {
+            newName.Text = oldName; 
+        }
+    }
+}
